a35dd5fb675590968754e0bee73a2b7265b90fe7
Faizalqossam <alqassamf@gmail.com>
2022-06-14 20:32:23 +0700

    refactor &  make new endpoints

--- src/utils/constants/endpoints.js
+++ src/utils/constants/endpoints.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 
-// const params = useParams();
+
 const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
@@ -9,8 +9,12 @@ const ENDPOINTS = {
     NOW_PLAYING: `${BASE_URL}/movie/now_playing?api_key=${API_KEY}`,
     TOP_RATED: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}`,
     TRANDING: `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`,
-    // DETAIL: `${BASE_URL}/movie/${params.id}?api_key=${API_KEY}&append_to_response=videos`,
-    // RECOMMENDATIONS: `${BASE_URL}/movie/${params.id}/recommendations?api_key=${API_KEY}`,
+    DETAIL(id){
+        return `${BASE_URL}/movie/${id}?api_key=${API_KEY}&append_to_response=videos`
+    },
+    RECOMMENDATIONS(id){
+        return `${BASE_URL}/movie/${id}/recommendations?api_key=${API_KEY}`
+    }
 }
 
 export default ENDPOINTS;
